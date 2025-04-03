08b836bc25dffdabe12611f6a61394d6bdea13c5
Luizotavio-Perina <luizotavio.perina@icloud.com>
2025-04-03 01:06:08 +0000

    fanalizado nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,4 @@
-#include <stdio.h>   // Desafio Super Trunfo - Países 
-// Tema 1 - Cadastro das Cartas 
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
+#include <stdio.h>
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -10,65 +6,68 @@ int main() {
     // Cadastrando as variáveis
     int populacao1, populacao2, np1, np2;
     float area1, area2, pib1, pib2;
+    float densidade1, densidade2, percapita1, percapita2;
     char codigocarta1[5], codigocarta2[5], cidade1[40], cidade2[40], estado1, estado2;
 
+    printf("BEM VINDO ao novo jogo SUPER TRUNFO\n");
+    printf("Cadastre suas cartas!\n");
 
-   // Frase de bem-vindo
-   printf("BEM VINDO ao novo jogo SUPER TRUNFO\n");
-   printf("Cadastre suas cartas!\n");
-
-    //Inserindo os dados da primeira carta
-    printf("CADASTRE A CARTA 1:\n");
+    // Cadastro da primeira carta
+    printf("\nCADASTRE A CARTA 1:\n");
 
     printf("Digite a letra que representa o estado (A-H): ");
     scanf(" %c", &estado1);
 
-    printf("A letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
+    printf("Código da Carta (ex: A01, B02): ");
     scanf(" %s", codigocarta1);
-    
-    getchar();
+
+    getchar();  // utilizei ogetchar para capturar o ENTER para evitar erro na leitura da string seguinte
     printf("Digite o nome da Cidade: ");
     scanf(" %[^\n]s", cidade1);
 
     printf("População: ");
-    scanf(" %d", &populacao1);
+    scanf("%d", &populacao1);
 
     printf("Área (em km²): ");
-    scanf(" %f", &area1);
+    scanf("%f", &area1);
 
     printf("PIB (em bilhões de reais): ");
-    scanf(" %f", &pib1);
+    scanf("%f", &pib1);
 
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &np1);
-    
-    
-    // Inserindo os dados da segunda carta
+    scanf("%d", &np1);
+
+    // Cadastro da segunda carta
     printf("\nCADASTRE A CARTA 2:\n");
 
     printf("Digite a letra que representa o estado (A-H): ");
     scanf(" %c", &estado2);
 
-    printf("A letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
+    printf("Código da Carta (ex: A01, B02): ");
     scanf(" %s", codigocarta2);
 
+    getchar();
     printf("Digite o nome da Cidade: ");
     scanf(" %[^\n]s", cidade2);
 
     printf("População: ");
-    scanf(" %d", &populacao2);
+    scanf("%d", &populacao2);
 
     printf("Área (em km²): ");
-    scanf(" %f", &area2);
+    scanf("%f", &area2);
 
     printf("PIB (em bilhões de reais): ");
-    scanf(" %f", &pib2);
+    scanf("%f", &pib2);
 
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &np2);
+    scanf("%d", &np2);
 
+    // Cálculo da Densidade Populacional e PIB per Capita
+    densidade1 = populacao1 / area1;
+    densidade2 = populacao2 / area2;
+    percapita1 = pib1 * 1000000000 / populacao1; 
+    percapita2 = pib2 * 1000000000 / populacao2;
 
-    
     // Exibição da primeira carta
     printf("\nCARTA 1:\n");
     printf("Estado: %c\n", estado1);
@@ -78,8 +77,10 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", np1);
-    
-    //Exibição da segunda carta
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", percapita1);
+
+    // Exibição da segunda carta
     printf("\nCARTA 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigocarta2);
@@ -88,6 +89,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", np2);
-    
-    return 0;  // Fim do programa
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", percapita2);
+
+    return 0;
 }
