0bef470947b4b98ce29cb307f8d742df4672759a
Luizotavio-Perina <luizotavio.perina@icloud.com>
2025-04-12 01:31:10 +0000

    Desafio novato tema 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
     float area1, area2, pib1, pib2;
     float densidade1, densidade2, percapita1, percapita2;
     float superpoder1, superpoder2;
-    char codigocarta1[5], codigocarta2[5], cidade1[40], cidade2[40], estado1, estado2;
+    char codigocarta1[5], codigocarta2[5], cidade1[40], cidade2[40], estado1[2], estado2[2];
 
     printf("BEM VINDO ao novo jogo SUPER TRUNFO\n");
     printf("Cadastre suas cartas!\n");
@@ -16,8 +16,8 @@ int main() {
     // Cadastro da primeira carta
     printf("\nCADASTRE A CARTA 1:\n");
 
-    printf("Digite a letra que representa o estado (A-H): ");
-    scanf(" %c", &estado1);
+    printf("Digite a sigla o ESTADO: ");
+    scanf(" %[^\n]s", estado1);
 
     printf("Código da Carta (ex: A01, B02): ");
     scanf(" %s", codigocarta1);
@@ -41,8 +41,8 @@ int main() {
     // Cadastro da segunda carta
     printf("\nCADASTRE A CARTA 2:\n");
 
-    printf("Digite a letra que representa o estado (A-H): ");
-    scanf(" %c", &estado2);
+    printf("Digite a sigla o ESTADO: ");
+    scanf(" %[^\n]s", estado2);
 
     printf("Código da Carta (ex: A01, B02): ");
     scanf(" %s", codigocarta2);
@@ -71,7 +71,7 @@ int main() {
 
     // Exibição da primeira carta
     printf("\nCARTA 1:\n");
-    printf("Estado: %c\n", estado1);
+    printf("Estado: %s\n", estado1);
     printf("Código: %s\n", codigocarta1);
     printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
@@ -83,7 +83,7 @@ int main() {
 
     // Exibição da segunda carta
     printf("\nCARTA 2:\n");
-    printf("Estado: %c\n", estado2);
+    printf("Estado: %s\n", estado2);
     printf("Código: %s\n", codigocarta2);
     printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
@@ -93,16 +93,41 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
     printf("PIB per Capita: %.2f reais\n", percapita2);
 
-    // Super poder calculado, mas não usado nesta etapa
+
+    // Calculando o super poder (desafio aventureiro tema 1)
     superpoder1 = (float)populacao1 + area1 + pib1 + np1 + percapita1 + (1.0 / densidade1);
     superpoder2 = (float)populacao2 + area2 + pib2 + np2 + percapita2 + (1.0 / densidade2);
 
-    // Comparação de cartas (Atributo: PIB)
-    printf("\nComparação de cartas (Atributo: PIB):\n\n");
 
-    printf("Carta 1 - %s (%c): %.2f bilhões de reais\n", cidade1, estado1, pib1);
-    printf("Carta 2 - %s (%c): %.2f bilhões de reais\n", cidade2, estado2, pib2);
+    // Comparações (desafio mestre tema 1)
+    printf("\nComparação de Cartas:\n");
+    printf("População: %s ; venceu (%d)\n", 
+           (populacao1 > populacao2) ? "Carta 1" : "Carta 2", 
+           populacao1 > populacao2);
+    printf("Área: %s venceu (%d)\n", 
+           (area1 > area2) ? "Carta 1" : "Carta 2", 
+           area1 > area2);
+    printf("PIB: %s venceu (%d)\n", 
+           (pib1 > pib2) ? "Carta 1" : "Carta 2", 
+           pib1 > pib2);
+    printf("Pontos Turísticos: %s venceu (%d)\n", 
+           (np1 > np2) ? "Carta 1" : "Carta 2", 
+           np1 > np2);
+    printf("Densidade Populacional: %s venceu (%d)\n", 
+           (densidade1 < densidade2) ? "Carta 1" : "Carta 2", 
+           densidade1 < densidade2); // menor vence
+    printf("PIB per Capita: %s venceu (%d)\n", 
+           (percapita1 > percapita2) ? "Carta 1" : "Carta 2", 
+           percapita1 > percapita2);
+    printf("Super Poder: %s venceu (%d)\n", 
+           (superpoder1 > superpoder2) ? "Carta 1" : "Carta 2", 
+           superpoder1 > superpoder2);
 
+    // Comparação de cartas (Atributo: PIB) (desafio novato tema 2)
+       printf("\nComparação de cartas (Atributo: PIB):\n\n");
+       printf("Carta 1 - %s (%s): %.2f bilhões de reais\n", cidade1, estado1, pib1);
+       printf("Carta 2 - %s (%s1): %.2f bilhões de reais\n", cidade2, estado2, pib2);
+    //  Comparação utilizando o if (desafio novato tema 2)
     if (pib1 > pib2) {
         printf("\nResultado: Carta 1 (%s) venceu!\n", cidade1);
     } else if (pib2 > pib1) {
